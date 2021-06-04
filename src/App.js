538ba9db443e5d0ce14da9f3c1ae9dcaa29ed502
可乐加糖 <33370733@qq.com>
2021-06-04 19:54:12 +0800

    初始化插件代码提交。

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css";
+import { Layout } from 'antd';
+const { Header, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <h3 className="logo">Q-Bookmark</h3>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div className="site-layout-content">Content</div>
+      </Content>
+    </Layout>
   );
 }
 
